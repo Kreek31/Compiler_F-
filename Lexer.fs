2365ed9d8f6a782f8ca33dc71a10b086fd738317
Kreek31 <misha.fedorov2004@gmail.com>
2024-10-03 16:54:49 +0300

    v1

---
+++ Lexer.fs
@@ -0,0 +1,73 @@
+﻿type Token = string
+type Expression = 
+    | Var of Token
+    | Abstraction of Token * Expression
+    | Application of Expression * Expression
+    | IntLiter of int
+    | IfThenElse of Expression * Expression * Expression
+    | LetIn of Token * Expression * Expression
+    | Recursion of Token * Expression * Expression
+    | FuncDefinition of Token
+    | Operator of Token * int * Expression list
+    | BracketExpr of Expression * Tree
+    | RecBracketExpr of Expression * Tree * Token
+and Tree = Map<Token, Expression>
+
+let operators = 
+    let binaryOperator f = function [IntLiter a; IntLiter b] -> IntLiter(f a b)
+    [
+        ("+", binaryOperator (+))
+        ("-", binaryOperator (-))
+        ("*", binaryOperator (*))
+        ("/", binaryOperator (/))
+        ("=", fun [IntLiter a; IntLiter b] -> if a = b then IntLiter 1 else IntLiter 0)
+        (">", fun [IntLiter a; IntLiter b] -> if a > b then IntLiter 1 else IntLiter 0)
+        ("<", fun [IntLiter a; IntLiter b] -> if a < b then IntLiter 1 else IntLiter 0)
+        ("<=", fun [IntLiter a; IntLiter b] -> if a <= b then IntLiter 1 else IntLiter 0)
+    ] |> Map.ofList
+
+let applyOperator op args =
+    match Map.tryFind op operators with
+    | Some f -> f args
+    | None -> failwithf "Unknown operator: %s" op
+
+let operatorArgCount = function
+    | "sin" -> 1
+    | "cos" -> 1
+    | "tg" -> 1
+    | _ -> 2
+
+let rec apply x y =
+    match x with 
+    | BracketExpr(Abstraction(v, e), env) -> calculateExpression e (Map.add v y env)
+    | RecBracketExpr(Abstraction(v, e), env, id) -> calculateExpression e (Map.add v y (Map.add id x env))
+    | Operator(op, n, args) when n = 1 -> applyOperator op (args @ [y])
+    | Operator(op, n, args) -> Operator(op, n - 1, args @ [y])
+
+and calculateExpression expr env =
+    match expr with
+    | Application(x, y) -> apply (calculateExpression x env) (calculateExpression y env)
+    | IntLiter n -> IntLiter n
+    | Var x -> Map.find x env
+    | FuncDefinition f -> Operator(f, operatorArgCount f, [])
+    | Operator(op, n, args) -> Operator(op, n, args)
+    | IfThenElse(e0, e1, e2) -> 
+        if calculateExpression e0 env = IntLiter 1 then calculateExpression e1 env else calculateExpression e2 env
+    | LetIn(id, e1, e2) -> 
+        let value = calculateExpression e1 env
+        calculateExpression e2 (Map.add id value env)
+    | Recursion(id, e1, e2) -> 
+        calculateExpression e2 (Map.add id (RecBracketExpr(e1, env, id)) env)
+    | Abstraction(_, _) -> BracketExpr(expr, env)
+    | BracketExpr(expr, _) -> expr
+
+let calculate exp = calculateExpression exp Map.empty
+
+// Примеры использования
+[ 
+    Application(Application(FuncDefinition "+", IntLiter 6), IntLiter 3)
+    Application(Abstraction("x", Var "x"), IntLiter 136)
+    LetIn("y", IntLiter 5, Application(Abstraction("x", Application(Application(FuncDefinition "+", Var "x"), Var "y")), IntLiter 1))
+    LetIn("id", Abstraction("x", Var "x"), LetIn("sq", Abstraction("z", Application(Application(FuncDefinition "*", Var "z"), Var "z")), Application(Var "sq", Application(Var "id", IntLiter 2))))
+]
+|> List.iter (fun expr -> printfn "Result = %A" (calculate expr))
---
+++ parser.fs
@@ -0,0 +1,81 @@
+﻿open FParsec
+
+type Identifier = string
+
+type Expression =
+    | Var of Identifier
+    | Abstraction of Identifier * Expression
+    | Application of Expression * Expression
+    | IntLiteral of int
+    | IfThenElse of Expression * Expression * Expression
+    | LetIn of Identifier * Expression * Expression
+    | Recursion of Identifier * Expression * Expression
+    | FuncDefinition of Identifier
+    | Operator of Identifier * int * Expression list
+    | BracketExpr of Expression * Map<Identifier, Expression>
+    | RecBracketExpr of Expression * Map<Identifier, Expression> * Identifier
+
+let ws = spaces
+let str_ws s = pstring s .>> ws
+let parenthesized p = between (str_ws "(") (str_ws ")") p
+
+let pIdentifier = many1Satisfy2 isAlphaNum |>> String.concat "" .>> ws
+let pIntLiteral = pint32 |>> IntLiteral
+
+let pVariable = pIdentifier |>> Var
+
+let pAbstraction = pipe3 (str_ws "\\" >>. pIdentifier) (str_ws "->") pExpression Abstraction
+
+let pApplication = 
+    pipe2 (choice [pAbstraction; pVariable; pIntLiteral; parenthesized pExpression])
+          (choice [pAbstraction; pVariable; pIntLiteral; parenthesized pExpression])
+          Application
+
+let pIfThenElse = 
+    pipe3 (str_ws "if" >>. pExpression)
+          (str_ws "then" >>. pExpression)
+          (str_ws "else" >>. pExpression)
+          IfThenElse
+
+let pLetIn =
+    pipe3 (str_ws "let" >>. pIdentifier .>> str_ws "=" >>. pExpression)
+          (str_ws "in" >>. pExpression)
+          LetIn
+
+let pRecursion =
+    pipe3 (str_ws "let rec" >>. pIdentifier .>> str_ws "=" >>. pAbstraction)
+          (str_ws "in" >>. pExpression)
+          Recursion
+
+let pFuncDefinition = pIdentifier |>> FuncDefinition
+
+let pOperator = pipe3 pFuncDefinition pint32 (sepBy pExpression ws) Operator
+
+let pBracketExpr =
+    pipe2 (str_ws "{" >>. pExpression) 
+          (str_ws "}" >>. many (pIdentifier .>> str_ws "=" >>. pExpression) |>> Map.ofList) 
+          BracketExpr
+
+let pRecBracketExpr =
+    pipe3 (str_ws "{" >>. str_ws "rec" >>. pIdentifier .>> str_ws "=" >>. pAbstraction)
+          (str_ws "}" >>. many (pIdentifier .>> str_ws "=" >>. pExpression) |>> Map.ofList)
+          RecBracketExpr
+
+let pExpression = choice [
+    pApplication
+    pIntLiteral
+    pVariable
+    pIfThenElse
+    pLetIn
+    pRecursion
+    pOperator
+    parenthesized pExpression
+    pBracketExpr
+    pRecBracketExpr
+]
+
+let parse str =
+    match run pExpression str with
+    | Success(result, _, _) -> result
+    | Failure(errorMsg, _, _) -> failwith errorMsg
+
